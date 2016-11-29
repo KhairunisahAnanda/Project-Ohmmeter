37861f81224e877bda5fef89a4f4b7b59b5a998a
Khairunisah ananda <khairunisah.ananda@yahoo.com>
2016-11-29 21:41:59 +0700

    Add files via upload

---
+++ HITUNG_RESISTOR.ino
@@ -0,0 +1,25 @@
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  digitalWrite(A0,0);
+  int nilaisensor = analogRead(A0);
+  
+  float Vtetap = nilaisensor * (5.0 / 1023.0);
+  Serial.println("NILAI TEGANGAN REFERENCE");
+  Serial.println(Vtetap);
+  
+  float Vr2=5.0 - Vtetap;
+  Serial.println("NILAI TEGANGAN PADA R2");
+  Serial.println(Vr2);
+  
+  float i =Vtetap / 100.0;
+  
+  float r = Vr2/i;
+  Serial.println("NILAI RESISTOR 2");
+  Serial.println(r);
+   
+}
+
+void loop(){   
+}
